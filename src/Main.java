d464679fbb0788b32e61f3de99a68d2c3117906f
jeevaa_the_coder <jeevaathecoder@gmail.com>
2023-07-23 17:22:07 +0530

    New Mesasages added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        System.out.println("3343434");
+        System.out.println(3434);
+
+        int a=34;
+        int b=34;
     }
 }
